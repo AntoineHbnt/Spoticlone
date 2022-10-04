93050c15cea305805b8cc8d279c3a6c174855d11
AntoineHbnt <49656644+AntoineHbnt@users.noreply.github.com>
2022-10-04 09:29:58 +0200

    chore(project): add eslint + prettier + autoimport + tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    AutoImport({ imports: ['react', 'react-router-dom'], dts: './src/auto-imports.d.ts' }),
+  ],
 });
