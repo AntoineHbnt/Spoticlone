720a6d9b42e830ea1b75edc4903479191ac95a28
AntoineHbnt <49656644+AntoineHbnt@users.noreply.github.com>
2022-10-05 18:56:18 +0200

    feat(search): path change on input change

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,13 @@ import { IconSVG } from '../icon/icon';
 
 export const Header = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const homeActive = location.pathname === '/';
+
+  const handleSearch = (e: InputEvent) => {
+    const { value } = e.target as HTMLInputElement;
+    value && navigate(`/search/${value}`);
+  };
 
   return (
     <header className="flex w-full items-center justify-between pl-2">
@@ -16,7 +22,11 @@ export const Header = () => {
             className="ri-xl flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-background-base text-white"
           />
         </NavLink>
-        <Input icon="search-line" placeholder="What do you want to listen to?" />
+        <Input
+          icon="search-line"
+          onChange={handleSearch}
+          placeholder="What do you want to listen to?"
+        />
       </div>
       <div className="h-12 w-12 rounded-full bg-background-base"></div>
     </header>
