3e3ed116f36c72ae954ffa83e51e9aff008365b8
AntoineHbnt <49656644+AntoineHbnt@users.noreply.github.com>
2022-10-17 11:34:01 +0200

    fix(artists): add unique key to child

--- src/components/component-shelf/artists.tsx
+++ src/components/component-shelf/artists.tsx
@@ -1,16 +1,28 @@
+import { Artist } from '../../types/Artist';
+
 export interface ArtistProps {
-  artists: any;
+  artists: Artist[];
 }
 
 export const Artists = (props: ArtistProps) => {
   const { artists } = props;
 
-  return artists.map((artist: any, index: number) => (
+  return (
     <>
-      <NavLink key={`artist-${index}`} to={`artist/${artist.id}`} className="hover:underline">
-        {artist.name}
-      </NavLink>
-      {index < artists.length - 1 ? ', ' : ''}
+      {artists.map((artist: Artist, index: number) => {
+        return (
+          <span key={artist.id}>
+            <NavLink
+              key={`artist-${artist.id + index}`}
+              to={`artist/${artist.id}`}
+              className="hover:underline"
+            >
+              {artist.name}
+            </NavLink>
+            {index < artists.length - 1 ? ', ' : ''}
+          </span>
+        );
+      })}
     </>
-  ));
+  );
 };
