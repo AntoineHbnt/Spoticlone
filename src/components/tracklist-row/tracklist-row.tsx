d3b9bc92991b051961e0081fed1d8d7be7a91c42
AntoineHbnt <49656644+AntoineHbnt@users.noreply.github.com>
2022-10-18 15:34:47 +0200

    feat(tracklist-row): works now as player when click on thumbnail

--- src/components/tracklist-row/tracklist-row.tsx
+++ src/components/tracklist-row/tracklist-row.tsx
@@ -1,6 +1,12 @@
+import { useState, useContext, useEffect } from 'react';
+import { PlayBackContext } from '../../context/PlaybackContext';
+import { usePausePlayback } from '../../hooks/player/use-pause-playback';
+import { useStartPlayback } from '../../hooks/player/use-start-playback';
 import { Track } from '../../types/Track';
 import { durationParser } from '../../utils/misc/duration';
+import { Icon } from '../icon/icon';
 import { ExplicitIcon } from '../icon/icons/explicit';
+import './tracklist-row.styles.scss';
 
 export interface TrackListRowProps {
   track: Track;
@@ -8,15 +14,39 @@ export interface TrackListRowProps {
 
 export const TrackListRow = (props: TrackListRowProps) => {
   const { track } = props;
+  const playback = useContext(PlayBackContext);
+  const itemUri: string = track.album.uri;
+  const isPlayingCondition: boolean = playback?.is_playing && playback?.context?.uri === itemUri;
 
-  console.log(props);
+  const [isPlaying, setIsPlaying] = useState<boolean>(isPlayingCondition);
+
+  const { mutateAsync: startPlayback } = useStartPlayback(itemUri, track.track_number - 1, 0);
+  const { mutateAsync: pausePlayback } = usePausePlayback();
+
+  useEffect(() => {
+    setIsPlaying(isPlayingCondition);
+  }, [playback]);
 
   return (
-    <div className="flex w-full items-center justify-between rounded p-2 pr-4 hover:bg-background-tinted-highlight">
+    <div
+      className={`trackListRow flex w-full items-center justify-between rounded p-2 pr-4 ${
+        isPlaying && 'active'
+      }`}
+    >
       <div className="flex items-center">
-        <img className="h-10 w-10" src={track.album.images[0].url} alt={track.album.name} />
+        <div className="relative h-10 w-10 shrink-0">
+          <img className="h-full w-full" src={track.album.images[0].url} alt={track.album.name} />
+          <button
+            onClick={() => (isPlaying ? pausePlayback() : startPlayback())}
+            className="absolute inset-0 z-10 flex h-full w-full items-center justify-center opacity-0"
+          >
+            <Icon name={`${isPlaying ? 'pause-fill' : 'play-fill'}  text-xl text-white`} />
+          </button>
+        </div>
         <div className="ml-4">
-          <p className="text-base text-white">{track.name}</p>
+          <p className={`text-base ${isPlaying ? 'text-spotify-green' : 'text-white'}`}>
+            {track.name}
+          </p>
           <p className="flex items-center text-sm text-gray-400">
             {track.explicit && <ExplicitIcon className="mr-2" />}
             {track.artists.map((artist) => artist.name).join(', ')}
