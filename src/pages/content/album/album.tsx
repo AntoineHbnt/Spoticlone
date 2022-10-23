f2c1271be6bfe3ad39c88b3c511b0df60f033ab3
AntoineHbnt <49656644+AntoineHbnt@users.noreply.github.com>
2022-10-23 23:47:53 +0200

    fix(album): change condition

--- src/pages/content/album/album.tsx
+++ src/pages/content/album/album.tsx
@@ -21,32 +21,32 @@ export const Album = () => {
 
   if (status === 'loading') return <div>Loading...</div>;
 
-  return (
-    album && (
-      <div className="flex w-full flex-col">
-        <Header data={album} />
-        <Content data={album} className="px-6 pb-8 lg:px-8">
-          <div className="mb-8">
-            <div className="mb-2 flex w-full border-b border-[hsla(0,0%,100%,.1)] p-2 text-xs text-[#b3b3b3]">
-              <span className="mr-4 flex w-10 items-center justify-center">#</span>
-              <span>TITLE</span>
-              <div className="mr-2 flex flex-1 justify-end">
-                <Icon width={16} fill={'#b3b3b3'} svg={IconSVG.Duration} />
-              </div>
+  return album ? (
+    <div className="flex w-full flex-col">
+      <Header data={album} />
+      <Content data={album} className="px-6 pb-8 lg:px-8">
+        <div className="mb-8">
+          <div className="mb-2 flex w-full border-b border-[hsla(0,0%,100%,.1)] p-2 text-xs text-[#b3b3b3]">
+            <span className="mr-4 flex w-10 items-center justify-center">#</span>
+            <span>TITLE</span>
+            <div className="mr-2 flex flex-1 justify-end">
+              <Icon width={16} fill={'#b3b3b3'} svg={IconSVG.Duration} />
             </div>
-            {album.tracks.items.map((elem, index) => {
-              const track: Track = {
-                ...elem,
-                album,
-                popularity: album.popularity,
-                external_ids: { isrc: '' },
-              };
-              return <TrackListRow track={track} index={index + 1} key={track.id} />;
-            })}
           </div>
-          {albums && <ComponentShelf title={`More by ${album.artists[0].name}`} data={albums} />}
-        </Content>
-      </div>
-    )
+          {album.tracks.items.map((elem, index) => {
+            const track: Track = {
+              ...elem,
+              album,
+              popularity: album.popularity,
+              external_ids: { isrc: '' },
+            };
+            return <TrackListRow track={track} index={index + 1} key={track.id} />;
+          })}
+        </div>
+        {albums && <ComponentShelf title={`More by ${album.artists[0].name}`} data={albums} />}
+      </Content>
+    </div>
+  ) : (
+    <>Album</>
   );
 };
