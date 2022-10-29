48d7783fa64e0a3141942a672586679e36b80516
AntoineHbnt <49656644+AntoineHbnt@users.noreply.github.com>
2022-10-29 16:00:03 +0200

    feat(hook): add use-artist-appears-on  hook

--- src/api/artist.ts
+++ src/api/artist.ts
@@ -1,13 +1,19 @@
+import { Album } from '../types/Album';
 import { Paging } from '../types/Paging';
 import { Track } from '../types/Track';
 import { apiSpotify } from '../utils/axios/axios';
 
-export const getArtistTopTracks = async (id: string): Promise<Track[]> => {
+export const getArtistTopTracks = async (id: string): Promise<Album[]> => {
   const response = await apiSpotify.get(`/artists/${id}/top-tracks?market=FR`);
   return response.data.tracks;
 };
 
 export const getArtistSingles = async (id: string): Promise<Paging<Track>> => {
   const response = await apiSpotify.get(`/artists/${id}/albums?include_groups=single`);
+  return response.data;
+};
+
+export const getArtistAppearson = async (id: string): Promise<Paging<Album>> => {
+  const response = await apiSpotify.get(`/artists/${id}/albums?include_groups=appears_on`);
   return response.data;
 };
