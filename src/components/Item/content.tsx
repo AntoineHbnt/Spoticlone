3fc47de628a429c1fd33376f01d7ce5df0e87f27
AntoineHbnt <49656644+AntoineHbnt@users.noreply.github.com>
2022-11-02 08:04:44 +0100

    fix(content): main component height

--- src/components/Item/content.tsx
+++ src/components/Item/content.tsx
@@ -15,13 +15,15 @@ export const Content = (props: ContentProps) => {
   const { data: color } = useVibrant(data.images[0].url);
 
   return (
-    <div className={`relative w-full  `} style={{ backgroundColor: color }}>
-      <div className="h-60 w-full bg-gradient-to-b from-[rgba(0,0,0,.6)] to-background-base" />
-      <div className="absolute inset-0 flex w-full flex-col">
+    <div className={`relative w-full `}>
+      <div className={`absolute inset-0 h-60 w-full `} style={{ backgroundColor: color }}>
+        <div className="h-full  w-full bg-gradient-to-b from-[rgba(0,0,0,.6)] to-background-base"></div>
+      </div>
+      <div className="flex h-fit w-full flex-col">
         <div className="hidden px-6 py-6 md:flex lg:px-8">
           <PlayButton uri={data.uri} width={56} />
         </div>
-        <div className={`max-w-[1955px]  ${className}`}>{children}</div>
+        <div className={`z-10  max-w-[1955px]  ${className}`}>{children}</div>
       </div>
     </div>
   );
